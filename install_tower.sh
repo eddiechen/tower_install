a985fb0b46591d7c78db7b46b2483210a9b57f9a
Eddie Chen <echen@Eddies-Air.chen.com>
2016-11-02 21:18:33 -0400

    modified

--- install_tower.sh
+++ install_tower.sh
@@ -11,7 +11,6 @@ done
 if [ $repos -eq 2 ]; then
     /usr/bin/ssh-keygen -f /root/.ssh/id_rsa -t rsa -N ''
     /usr/bin/ssh-copy-id root@localhost
-    #/usr/sbin/subscription-manager repos --disable=rhel-7-server-extras-rpms --disable=rhel-7-server-optional-rpms --disable=rhel-7-server-rh-common-rpms
     /usr/bin/yum install -y wget git
     /usr/bin/wget -P /tmp https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm 
     /usr/bin/rpm -i /tmp/epel-release-latest-7.noarch.rpm
@@ -19,4 +18,6 @@ if [ $repos -eq 2 ]; then
     /usr/bin/git clone https://github.com/eddiechen/ansible_playbooks.git /opt/ansible_playbooks
     /usr/bin/ansible-playbook -i "localhost," /opt/ansible_playbooks/playbooks/tower-install.yml
     rm -rf /opt/ansible_playbooks /root/install_tower.sh
+else
+    echo "Please verify that \"RHEL Extras\" and \"RHEL Optional\" yum repositories are available"
 fi
